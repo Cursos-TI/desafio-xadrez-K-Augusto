0d842bd255ce92155134c211c50bcc948d647248
K-Augusto <202502643021@alunos.estacio.br>
2025-05-25 19:00:14 +0000

    codificacao das peças torre e bispo

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,24 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    //Movimentação da Torre usando a estrutura de repetição for
+
+    printf("Movimentação do tabuleiro de xadrez, peça - Torre.:\n\n");
+
+    for(int i = 1; i <= 5; i++){
+        printf("Direita\n");
+    }
+
+    //Movimentação do Bispo usando a estrutura de repetição while
+
+    printf("\nMovimentação do tabuleiro de xadrez, peça - Bispo.:\n\n");
+
+    int j = 1;
+
+    while(j <= 5){
+        printf("Cima, Direita\n");
+        j++;
+    }
+
     return 0;
 }
